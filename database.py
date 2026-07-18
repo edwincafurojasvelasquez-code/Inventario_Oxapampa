48cd4b0968d2a70ea364fead1a39e38a992c7ad2
Edwin Rojas <edwincafurojasvelasquez@gmail.com>
2026-07-17 19:38:04 -0500

    Configurar conexion y creacion de tablas productos y usuarios en SQLite

--- database.py
+++ database.py
@@ -18,5 +18,42 @@ def crear_tabla():
     )
     """)
 
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario TEXT NOT NULL UNIQUE,
+        contrasena TEXT NOT NULL
+    )
+    """)
+
     conexion.commit()
     conexion.close()
+
+def crear_usuario_admin():
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute("SELECT * FROM usuarios WHERE usuario = ?", ("admin",))
+    existe = cursor.fetchone()
+
+    if not existe:
+        cursor.execute(
+            "INSERT INTO usuarios (usuario, contrasena) VALUES (?, ?)",
+            ("admin", "123456")
+        )
+        conexion.commit()
+
+    conexion.close()
+
+def verificar_usuario(usuario, contrasena):
+    conexion = conectar()
+    cursor = conexion.cursor()
+
+    cursor.execute(
+        "SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?",
+        (usuario, contrasena)
+    )
+    resultado = cursor.fetchone()
+    conexion.close()
+
+    return resultado is not None
